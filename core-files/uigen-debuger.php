5be25fe382864cc2f537f94f7e22fc588ddd1454
dadmor <dadmor@gmail.com>
2014-05-08 12:57:21 +0200

    Debuger: add pages - new interface

--- core-files/uigen-debuger.php
+++ core-files/uigen-debuger.php
@@ -147,28 +147,69 @@ function decorate_debuged_page_header($gridName,$args){
 
 		<div id="pages_creator">
 			<h1>Pages Creator (not available yet)</h1>
-			<div style="margin-top:20px; margin-bottom:60px; padding-bottom:40px; border-bottom:1px solid #ccc;">
 
-				<a class="btn btn-primary btn-lg" role="button">
-					<div style="float:left; text-align:center; margin-top:10px;">
-						<span class="glyphicon glyphicon-file" style="font-size:90px"></span><br/>
-						<p>Page1</p>
-					</div>
-				</a>
-				<a class="btn btn-primary btn-lg" role="button">
-					<div style="float:left; text-align:center; margin-top:10px;">
-						<span class="glyphicon glyphicon-file" style="font-size:90px"></span><br/>
-						<p>Page2</p>
-					</div>
-				</a>
-				<a class="btn btn-primary btn-lg" role="button">
-					<div style="float:left; text-align:center; margin-top:10px;">
-						<span class="glyphicon glyphicon-file" style="font-size:90px"></span><br/>
-						<p>Page3</p>
-					</div>
-				</a>				
-				<br style="clear:both"/>
+			<div class="panel panel-primary" style="width:200px; float:left; margin-right:10px;">
+				<div class="panel-heading"> 
+			  		<span class="glyphicon glyphicon-briefcase"></span>
+			  		Ośrodki
+				</div>
+				<!-- <div class="panel-body"></div> -->
+				<ul class="list-group">
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">View</a></li>	    
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">Form</a></li>
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">List</a></li>
+				</ul>
+				<div class="panel-footer"><a href="#">More options</a></div>
 			</div>
+
+			<div class="panel panel-primary" style="width:200px; float:left;">
+				<div class="panel-heading"> 
+			  		<span class="glyphicon glyphicon-briefcase"></span>
+			  		Ośrodki
+				</div>
+				<!-- <div class="panel-body"></div> -->
+				<ul class="list-group">
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">View</a></li>	    
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">Form</a></li>
+				    <li class="list-group-item"><span class="glyphicon glyphicon-file"></span> <a href="#">List</a></li>
+				</ul>
+				<div class="panel-footer"><a href="#">More options</a></div>
+			</div>
+
+			
+			
+			<br style="clear:both"/>	
+			<button type="button" class="btn btn-primary btn-lg" style="">
+				<span class="glyphicon glyphicon-plus"></span><span class="glyphicon glyphicon-file"></span> Add new page
+			</button>
+			<button type="button" class="btn btn-primary btn-lg" style="">
+				<span class="glyphicon glyphicon-plus"></span><span class="glyphicon glyphicon-briefcase"></span> Add new posttype
+			</button>
+			<button type="button" class="btn btn-primary btn-lg" style="">
+				<span class="glyphicon glyphicon-plus"></span><span class="glyphicon glyphicon-user"></span> Add new users
+			</button>
+			<br style="clear:both"/>	
+
+			<div class="panel panel-primary" style="">
+				<div class="panel-heading"> 
+			  		<span class="glyphicon glyphicon-briefcase"></span>
+			  		Add new object
+				</div>
+				<div class="panel-body">
+					<div class="form-group">
+					    <label for="exampleInputEmail1">Object name</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter object name">
+					  </div>
+				</div>		
+				<div class="panel-footer">
+					<button type="button" class="btn btn-primary" style="float:right">
+						<span class="glyphicon glyphicon-plus"></span><span class="glyphicon glyphicon-file"></span> Add new object
+					</button>
+					<br style="clear:both"/>	
+				</div>
+			</div>
+
+			<br style="clear:both"/>	
 		</div>
 
 		<span class="glyphicon glyphicon-th-large"></span> 
